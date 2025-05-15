5e82ca7ad92bf36b6ad5af3a9b78f2fbe3c5c115
Vinicius <vini9296@gmail.com>
2025-05-15 19:03:23 -0300

    teste

---
+++ src/Casting.java
@@ -0,0 +1,24 @@
+public class Casting {
+    public static void main(String[] args) {
+        String titulo = "O Pequeno Príncipe";
+        String autor = "Antoine de Saint-Exupéry";
+        int numeroDePaginas = 96;
+        double preco = 39.99;
+        char categoria = 'F';
+
+        String categoriaDescricao = "";
+        if (categoria == 'F'){
+            categoriaDescricao = "Ficção";
+        } else if (categoria == 'N') {
+            categoriaDescricao = "Não-Ficção";
+        } else if (categoria == 'T') {
+            categoriaDescricao = "Tecnologia";
+        } else if (categoria == 'H') {
+            categoriaDescricao = "História";
+        }
+        System.out.println("Livro cadastrado: \"" + titulo + "\", de " + autor + ". Ele possui " + numeroDePaginas + " páginas, custa R$" + preco + " e pertence à categoria " + categoriaDescricao + ".");
+    }
+    }
+
+
+
